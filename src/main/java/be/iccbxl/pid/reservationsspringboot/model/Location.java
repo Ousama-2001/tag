c9b9315425a6ed3e2ec89dcbfb6a95aa303a3f94
Betteranh <danguyen.1129@gmail.com>
2025-01-05 21:09:08 +0100

    Modèle Show +Relation ManyToOne with Location

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Location.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Location.java
@@ -3,6 +3,9 @@ package be.iccbxl.pid.reservationsspringboot.model;
 import com.github.slugify.Slugify;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "locations")
 public class Location {
@@ -23,18 +26,22 @@ public class Location {
     private String website;
     private String phone;
 
+    @OneToMany(targetEntity = Show.class, mappedBy = "location")
+    private List<Show> shows = new ArrayList<>();
+
+
     protected Location() {
     }
 
-    public Location(String slug, String designation, String address, Locality locality, String website, String phone) {
-        Slugify slg = new Slugify();
-
-        this.slug = slg.slugify(designation);
+    public Location(Long id, String slug, String designation, String address, Locality locality, String website, String phone, List<Show> shows) {
+        this.id = id;
+        this.slug = slug;
         this.designation = designation;
         this.address = address;
         this.locality = locality;
         this.website = website;
         this.phone = phone;
+        this.shows = shows;
     }
 
     public Long getId() {
@@ -95,11 +102,41 @@ public class Location {
         this.phone = phone;
     }
 
-    @Override
-    public String toString() {
-        return "Location [id=" + id + ", slug=" + slug + ", designation=" + designation
-                + ", address=" + address + ", locality=" + locality + ", website="
-                + website + ", phone=" + phone + "]";
+    public List<Show> getShows() {
+        return shows;
     }
 
+    public Location addShow(Show show) {
+        if (!this.shows.contains(show)) {
+            this.shows.add(show);
+            show.setLocation(this);
+        }
+
+        return this;
+    }
+
+    public Location removeShow(Show show) {
+        if (this.shows.contains(show)) {
+            this.shows.remove(show);
+            if (show.getLocation().equals(this)) {
+                show.setLocation(null);
+            }
+        }
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", slug='" + slug + '\'' +
+                ", designation='" + designation + '\'' +
+                ", address='" + address + '\'' +
+                ", locality=" + locality +
+                ", website='" + website + '\'' +
+                ", phone='" + phone + '\'' +
+                ", shows=" + shows +
+                '}';
+    }
 }
