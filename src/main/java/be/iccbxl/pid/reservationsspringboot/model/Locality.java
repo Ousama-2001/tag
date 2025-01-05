7969dbcd2a5882d3428a09438af1e8ebb91d02e5
Betteranh <danguyen.1129@gmail.com>
2025-01-05 20:29:50 +0100

    Controller Locality +template +route

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Locality.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Locality.java
@@ -1,34 +1,57 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "localities")
 public class Locality {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String postalCode;
     private String locality;
 
-    @OneToMany( targetEntity=Location.class, mappedBy="locality" )
+    // @OneToMany(targetEntity = Locality.class, mappedBy = "locality")
+    @OneToMany(mappedBy = "locality")
     private List<Location> locations = new ArrayList<>();
 
+    protected Locality() {
+    }
+
+    public Locality(String postalCode, String locality) {
+        this.postalCode = postalCode;
+        this.locality = locality;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
 
     public List<Location> getLocations() {
         return locations;
     }
 
     public Locality addLocation(Location location) {
-        if(!this.locations.contains(location)) {
+        if (!this.locations.contains(location)) {
             this.locations.add(location);
             location.setLocality(this);
         }
@@ -37,14 +60,19 @@ public class Locality {
     }
 
     public Locality removeLocation(Location location) {
-        if(this.locations.contains(location)) {
+        if (this.locations.contains(location)) {
             this.locations.remove(location);
-            if(location.getLocality().equals(this)) {
+            if (location.getLocality().equals(this)) {
                 location.setLocality(null);
             }
         }
 
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Locality [id=" + id + ", postalCode=" + postalCode + ", locality=" + locality + "]";
     }
 
 }
