90273708b7168153301be845e5a305918bb0aabf
Betteranh <danguyen.1129@gmail.com>
2025-01-07 22:51:03 +0100

    Relation ManyToMany between User & Representation

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/User.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/User.java
@@ -32,6 +32,9 @@ public class User {
     @ManyToMany(mappedBy = "users")
     private List<Role> roles = new ArrayList<>();
 
+    @ManyToMany(mappedBy = "users")
+    private List<Representation> representations = new ArrayList<>();
+
     // Problème avec lombok !
     public String getPassword() {
         return password;
@@ -58,6 +61,28 @@ public class User {
         if (this.roles.contains(role)) {
             this.roles.remove(role);
             role.getUsers().remove(this);
+        }
+
+        return this;
+    }
+
+    public List<Representation> getRepresentations() {
+        return representations;
+    }
+
+    public User addRepresentation(Representation representation) {
+        if (!this.representations.contains(representation)) {
+            this.representations.add(representation);
+            representation.addUser(this);
+        }
+
+        return this;
+    }
+
+    public User removeRepresentation(Representation representation) {
+        if (this.representations.contains(representation)) {
+            this.representations.remove(representation);
+            representation.getUsers().remove(this);
         }
 
         return this;
