15a49b08250ac827b24664e9f0eb993f37b60eea
Betteranh <danguyen.1129@gmail.com>
2025-01-07 22:46:28 +0100

    Added UserRepository & UserService

--- src/main/java/be/iccbxl/pid/reservationsspringboot/repository/UserRepository.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/repository/UserRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByLogin(String login);
     List<User> findByLastname(String lastname);
 
     User findById(long id);
+
+    User findByLogin(String login);
+
+    User findByEmail(String email);
 }
