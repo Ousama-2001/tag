3783091feb75cc39a742521ebec84c850ddaebaa
Betteranh <danguyen.1129@gmail.com>
2025-01-15 03:12:50 +0100

    Relation Reservation et RepresentationReservation

---
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/service/ReservationService.java
@@ -0,0 +1,38 @@
+package be.iccbxl.pid.reservationsspringboot.service;
+
+import be.iccbxl.pid.reservationsspringboot.model.Reservation;
+import be.iccbxl.pid.reservationsspringboot.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReservationService {
+    @Autowired
+    private ReservationRepository repository;
+
+    public List<Reservation> getAllReservations() {
+        List<Reservation> reservations = new ArrayList<>();
+        repository.findAll().forEach(reservations::add);
+        return reservations;
+    }
+
+    public Reservation getReservation(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void addReservation(Reservation reservation) {
+        repository.save(reservation);
+    }
+
+    public void updateReservation(Long id, Reservation reservation) {
+        repository.save(reservation);
+    }
+
+    public void deleteReservation(Long id) {
+        repository.deleteById(id);
+    }
+}
