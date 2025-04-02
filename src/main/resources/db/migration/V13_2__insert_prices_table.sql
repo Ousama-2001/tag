93a67082c113e3ec462eba0b3063180ce9f3d060
Betteranh <danguyen.1129@gmail.com>
2025-04-02 20:56:24 +0200

    Correction Représentation et Show + MaJ index.html et show.html + prices

---
+++ src/main/resources/db/migration/V13_2__insert_prices_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO `prices` (`type`, `price`, `start_date`, `end_date`)
+VALUES
+-- Pour Ayiti
+('Tarif plein - Ayiti', 18.00, '2025-01-01', '2025-12-31'),
+('Tarif réduit - Ayiti', 12.00, '2025-01-01', '2025-12-31'),
+
+-- Pour Cible mouvante
+('Tarif plein - Cible mouvante', 20.00, '2025-01-01', '2025-12-31'),
+('Étudiant - Cible mouvante', 10.00, '2025-01-01', '2025-12-31'),
+
+-- Pour Ceci n’est pas un chanteur belge
+('Standard - Chanteur belge', 15.00, '2025-01-01', '2025-12-31'),
+('Senior - Chanteur belge', 12.00, '2025-01-01', '2025-12-31'),
+
+-- Pour Manneke
+('Tarif normal - Manneke', 17.00, '2025-01-01', '2025-12-31'),
+('Tarif enfant - Manneke', 8.00, '2025-01-01', '2025-12-31');
---
+++ src/main/resources/db/migration/V16_2__insert_show_price_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO `show_price` (`show_id`, `price_id`)
+VALUES
+-- Ayiti
+(1, 1),
+(1, 2),
+
+-- Cible mouvante
+(2, 3),
+(2, 4),
+
+-- Ceci n'est pas un chanteur belge
+(3, 5),
+(3, 6),
+
+-- Manneke
+(4, 7),
+(4, 8);
